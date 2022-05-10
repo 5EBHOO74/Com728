1e03c1667fd43b5451fb48fbe86f8fccab82f7f8
5ebhoo74 <5ebhoo74@solent.ac.uk>
2022-05-10 13:20:58 +0100

    Created a basic input/output program.

---
+++ basics/input/review.py
@@ -0,0 +1,14 @@
+# Ask user for eye character
+print("Please enter an eye character:")
+eye = input()
+
+# Ask user for the length of the glasses
+print("Please enter the length of the glasses:")
+length = int(input())
+
+
+print(f"#########{' ' * length}#########")
+print(f"#   {eye}   {'#' * (length + 2)}   {eye}   #")
+print(f"#########{' ' * length}#########")
+
+print()
